86d2384ab6295f67a9f00899845742d4563d5060
nilesh yadav <nileshyadav1788@rediffmail.com>
2025-08-02 20:45:58 +0530

    move local to remote

---
+++ ApiTests/getPetById.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('SuiteName: getPetById', function() {
+  it('should fetch pet details by id', async function() {
+    const endpoint = 'https://petstore.swagger.io/v2/pet/10';
+    try {
+      const response = await axios.get(endpoint);
+      expect(response.status).to.equal(200);
+      expect(response.data.id).to.equal(10);
+      expect(response.data.name).to.equal('doggie');
+    } catch (error) {
+      console.error('Test failed:', error);
+      throw error; // Stop execution if error
+    }
+  });
+});
